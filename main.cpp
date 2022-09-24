0d711bcd501c5122c90c6bcd89c1e5d7612bd318
Caelan Neumann <caelann2@gmail.com>
2022-09-23 17:51:06 -0700

    Update main.cpp
    create rectangle class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Rectangle Creator!" << std::endl;
+    
     return 0;
 }
+
+
+class Rectangle {
+private:
+    double length;
+    double width;
+public:
+    void set_dimensions(double l, double w){
+        length = l;
+        width = w;
+    }
+
+    void set_width(double w){
+        width = w;
+    }
+
+    void set_length(double l){
+        length = l;
+    }
+
+    double get_width() const{
+        return width;
+    }
+
+    double get_length() const{
+        return length;
+    }
+};
