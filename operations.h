be4210b93aa465c3c194714a20a3f6af0f48c329
Caelan Neumann <caelann2@gmail.com>
2022-10-06 23:01:35 -0600

    Create operations.h
    create header for operations

---
+++ operations.h
@@ -0,0 +1,12 @@
+//
+// Created by Caelan Neumann on 2022-10-06.
+//
+
+#ifndef LAB2_CAELAN_OPERATIONS_H
+int addTwo(int x, int y);
+int subtractTwo(int x, int y);
+int divideTwo(int x, int y);
+int multiplyTwo(int x, int y);
+#define LAB2_CAELAN_OPERATIONS_H
+
+#endif //LAB2_CAELAN_OPERATIONS_H
