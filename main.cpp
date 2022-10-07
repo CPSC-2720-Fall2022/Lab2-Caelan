fcbe886b0c7f5c58bb38768284775c1d222a98c5
Caelan Neumann <caelann2@gmail.com>
2022-10-06 22:20:29 -0600

    Update main.cpp
    update names of functions

--- main.cpp
+++ main.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 
-
 int addTwo (int x , int y) {
     return x + y;
 }
 
-int subtractTwoNumbers(int x, int y) {
+int subtractTwo(int x, int y) {
     return x - y;
 }
 
-int divideTwoNumbers(int x, int y) {
+int divideTwo(int x, int y) {
     if (y == 0){
         std::cout << "Can't Divide by Zero" << std::endl;
         return 0;
     }
     return x/y;
 }
+
 int multiplyTwo(int x, int y) {
     return x * y;
 }
@@ -28,8 +28,8 @@ int main() {
     std::cin >> x;
 
     std::cout << "Sum: " << addTwo(x,y) << std::endl
-    << "Difference: " << subtractTwoNumbers(x, y) << std::endl
+    << "Difference: " << subtractTwo(x, y) << std::endl
     << "Product: " << multiplyTwo(x, y) << std::endl
-    << "Quotient " << divideTwoNumbers(x, y);
+    << "Quotient " << divideTwo(x, y);
     return 0;
 }
